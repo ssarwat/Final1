a909f69b239a0617c0cff2630d9a182dd9f6fba2
ssarwat <sumaiyah_sarwat@yahoo.com>
2019-08-23 13:00:03 -0400

    adding update,detail, and delete

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,6 +43,25 @@ public class HomeController {
             return "messagepage";
         }
         messagesRepository.save(message);
+        return "redirect:/";
+    }
+
+
+    @RequestMapping("/detail/{id}")
+    public String detailMessage(@PathVariable("id") long id, Model model){
+        model.addAttribute("message", messagesRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateMessage(@PathVariable("id") long id, Model model){
+        model.addAttribute("message", messagesRepository.findById(id).get());
+        return "messagepage";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteMessage(@PathVariable("id") long id, Model model){
+        messagesRepository.deleteById(id);
         return "redirect:/";
     }
 
